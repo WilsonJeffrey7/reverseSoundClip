0f251e77762060acba6b37eec629ec607724b074
WilsonJeffrey7 <90283258+WilsonJeffrey7@users.noreply.github.com>
2022-08-14 15:21:58 -0500

    Add files via upload

---
+++ ArrayStack.java
@@ -0,0 +1,64 @@
+// Jeffrey Wilson
+// 2/25/22
+// CS 3345.503
+// Homework 1
+
+import java.util.EmptyStackException;
+
+public class ArrayStack implements BKStack {
+
+    //Contains the necessary stack elements
+    private double[] elements = new double[10];
+
+    //Displays the current stack amount
+    private int stackCurrent = 0;
+
+    //This function pushes Num to the top of the stock
+    public void push(double Num) {
+        if (stackCurrent == elements.length) {
+        //Checks if the array is full, and if it is, copy and
+            //paste the content into a bigger array
+            double[] bigNum = new double[2 * elements.length];
+
+            for(int i=0; i < elements.length; i++)
+            {
+                bigNum[i] = elements[i];
+            }
+            elements = bigNum;
+        }
+        //Move num to the next open are in stackCurrent
+        elements[stackCurrent] = Num;
+        //Increment the number of elements by 1
+        stackCurrent++;
+    }
+
+    //This function removes the top element and returns it
+    //It will then check if the stack is empty, and throws an
+    //exception if it is empty upon call
+    public double pop() {
+        if (stackCurrent == 0)
+            throw new EmptyStackException();
+        //Identifies the top element in the stack
+        double topElem = elements[stackCurrent - 1];
+        //Decrement the num of elements on the stack
+        stackCurrent--;
+        return topElem;
+    }
+
+    //Check whether the stack is empty and return true or false
+    //based on the stackCurrent
+    public boolean isEmpty() {
+        return (stackCurrent == 0);
+    }
+
+    //Fetch the first element from the stack
+    @Override
+    public double peek() {
+        if ( stackCurrent == 0 )
+            throw new EmptyStackException();
+        //Checks the top element in the stack
+        double topElem = elements[stackCurrent - 1];
+        return topElem;
+    }
+
+}
---
+++ ListStack.java
@@ -0,0 +1,62 @@
+// Jeffrey Wilson
+// 2/25/22
+// CS 3345.503
+// Homework 1
+
+import java.util.EmptyStackException;
+
+public class ListStack implements BKStack{
+
+    //Node that holds an element in the linked list
+        // that corresponds to the stack
+    private static class Node {
+        double element;
+        Node next;
+    }
+    //Points to the node at the top of the stack,
+    //then checks if the stack is empty if it
+    //is null
+    private Node top;
+
+    //Push Num to the top of the stack
+    public void push(double Num) {
+        //Node holds the new top element
+        Node newTop;
+        newTop = new Node();
+        //Num is stored in the new node
+        newTop.element = Num;
+        //The new node points to where the old top was
+        newTop.next = top;
+        //New element now points to the top of the stack
+        top = newTop;
+    }
+
+    //Checks the top element of the stack and returns it
+    //Exception is thrown if the stack is empty
+    public double pop() {
+        if (top == null)
+            throw new EmptyStackException();
+        //The topElem is being popped from the stack
+        double topElem = top.element;
+        //The previous element is now placed at the top
+        top = top.next;
+        return topElem;
+    }
+
+    //Boolean statement to determine if the stack
+    //is empty or not and returns null based on it
+    public boolean isEmpty() {
+        return (top == null);
+    }
+
+    //Fetch the first element from the stack
+    @Override
+    public double peek() {
+        if ( top == null )
+            throw new EmptyStackException();
+        //Pops out the top elem if not null
+        double topElem = top.element;
+        return topElem;
+    }
+
+}
